8f1c08a71fc6255a34b40b44a73c36abc5ee9b7b
Stanislav Zeman <zeman@standa.dev>
2023-12-01 21:17:29 +0100

    feat: add file server handler

--- main.go
+++ main.go
@@ -8,12 +8,16 @@ import (
 const (
 	hostname = "localhost"
 	port = "8080"
+
+	webFilesDirectory = "static/"
 )
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(webFilesDirectory)))
 	corsMux := middlewareCors(mux)
 
 	addr := fmt.Sprintf("%s:%s", hostname, port)
+	fmt.Printf("Running server on %s\n", addr)
 	http.ListenAndServe(addr, corsMux)
 }
