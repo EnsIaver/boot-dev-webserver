aee05c837ac0caf066cfc358b1bfbae6707ea876
Stanislav Zeman <zeman@standa.dev>
2023-12-04 20:56:55 +0100

    feat: implement health check handler

--- main.go
+++ main.go
@@ -2,22 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 const (
 	hostname = "localhost"
-	port = "8080"
+	port     = "8080"
 
-	webFilesDirectory = "static/"
+	webFilesDirectory = "static"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(webFilesDirectory)))
+	dir := http.Dir(webFilesDirectory)
+	fs := http.FileServer(dir)
+
+	mux.Handle("/app/", http.StripPrefix("/app", fs))
+	mux.HandleFunc("/healthz", healthCheck)
+
 	corsMux := middlewareCors(mux)
+	loggingMux := middlewareLogging(corsMux)
 
 	addr := fmt.Sprintf("%s:%s", hostname, port)
-	fmt.Printf("Running server on %s\n", addr)
-	http.ListenAndServe(addr, corsMux)
+	fmt.Printf("Running server on %s...\n", addr)
+	err := http.ListenAndServe(addr, loggingMux)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Shutting down server...")
 }
