9fedbe401d9bd6cb35c833178a8eab69a6a966b7
Stanislav Zeman <zeman@standa.dev>
2023-12-25 23:33:39 +0100

    feat(main): add storage initialization to main

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -5,11 +5,15 @@ import (
 	"log"
 	"net/http"
 
-	"git.standa.dev/boot-dev-webserver/pkg/handlers"
-	"git.standa.dev/boot-dev-webserver/pkg/config"
-	"git.standa.dev/boot-dev-webserver/pkg/fileserver"
-	"git.standa.dev/boot-dev-webserver/pkg/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+
+	"git.standa.dev/boot-dev-webserver/pkg/config"
+	"git.standa.dev/boot-dev-webserver/pkg/database"
+	"git.standa.dev/boot-dev-webserver/pkg/fileserver"
+	"git.standa.dev/boot-dev-webserver/pkg/handlers"
+	"git.standa.dev/boot-dev-webserver/pkg/middleware"
 )
 
 const (
@@ -19,18 +23,31 @@ const (
 
 func main() {
 	r := chi.NewRouter()
-	cfg := &config.ApiConfig{}
+	db, err := sqlx.Open("sqlite3", ":memory:")
+	if err != nil {
+		log.Fatalf("failed creating a sqlite db: %v", err)
+	}
+
+	storage := database.NewSQLiteStorage(db)
+	err = storage.Initialize()
+	if err != nil {
+		log.Fatalf("failed initialization sqlite db: %v", err)
+	}
+
+	cfg := &config.Config{
+		Storage: database.NewSQLiteStorage(db),
+	}
 	r.Mount("/api", handlers.NewApiRouter(cfg))
 	r.Mount("/app", fileserver.NewRouter(cfg))
 	r.Mount("/admin", handlers.NewAdminRouter(cfg))
 	handler := middleware.Wrap(r)
 
 	addr := fmt.Sprintf("%s:%s", hostname, port)
-	fmt.Printf("Running server on %s...\n", addr)
-	err := http.ListenAndServe(addr, handler)
+	log.Printf("Running server on %s...\n", addr)
+	err = http.ListenAndServe(addr, handler)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatalf("failed running the webserver: %v", err)
 	}
 
-	fmt.Println("Shutting down server...")
+	log.Println("Shutting down server...")
 }
